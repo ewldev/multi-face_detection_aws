c7e5fba72db27c674b713aa4d81068720b311efa
eleung32 <52718704+eleung32@users.noreply.github.com>
2021-03-30 17:17:55 -0400

    fixed back ticks for facerecognition js

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -5,9 +5,9 @@ const FaceRecognition = ({ imageUrl, boxes }) => {
   return (
     <div className='center ma'>
       <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
+        <img id='inputimage' alt='' src={imageUrl} width='500px' height='auto'/>
         {boxes.map(box =>
-          <div key={'box${box.topRow}${box.rightCol}'}
+          <div key={`box${box.topRow}${box.rightCol}`}
             className='bounding-box'
             style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}>
           </div>
